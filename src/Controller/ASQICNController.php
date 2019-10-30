335bf1891cb0e01f7160169f7eca0a6a0e2c3995
Maxime Maréchal <maxime.marechal@supinternet.fr>
2019-10-30 14:41:51 +0100

    Added map bound

--- src/Controller/ASQICNController.php
+++ src/Controller/ASQICNController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Service\ASQICNAPI;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class ASQICNController
+ * @package App\Controller
+ * @Route("/ASQICN")
+ */
 class ASQICNController extends AbstractController
     {
     /**
-    * @Route("/test", name="ASQICN_test")
+    * @Route("/map/bound", name="ASQICN_map_bound")
     */
-    public function test()
+    public function mapBound()
     {
-        return $this->json(['test' => 'test']);
+        $ASQICNAPI = new ASQICNAPI();
+        $result = $ASQICNAPI->mapBound(39.379436,116.091230,40.235643,116.784382);
+        return $this->json($result);
     }
 }
---
+++ src/Service/ASQICNAPI.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Service;
+
+use Exception;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+
+class ASQICNAPI {
+
+    private const API_KEY = '79c79e7f80b7fecaf745d71a69aa145b9d790043';
+
+    private const API_ENDPOINT = 'https://api.waqi.info';
+
+    /**
+     * @param string $route
+     * @param array $params
+     * format url with $route, API_ENDPOINT and API_KEY
+     * @return string
+     */
+    private function createUrl(string $route, array $params)
+    {
+        $url = self::API_ENDPOINT . $route . '/?token=' . self::API_KEY;
+        foreach ($params as $key => $value) {
+            $url .= "&$key=$value";
+        }
+        return $url;
+    }
+
+    /**
+     * @param float $lat1
+     * @param float $lng1
+     * @param float $lat2
+     * @param float $lng2
+     * @return mixed
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * Make a request to /map/bounds
+     */
+    public function mapBound(float $lat1, float $lng1, float $lat2, float $lng2)
+    {
+        $httpClient = HttpClient::create();
+        try {
+            $response = $httpClient->request('GET', $this->createUrl('/map/bounds', ['latlng' => "$lat1,$lng1,$lat2,$lng2"]));
+            try {
+                return json_decode($response->getContent(), true);
+            } catch (Exception $e) {
+                var_dump($e);
+            }
+        } catch (TransportExceptionInterface $e) {
+            var_dump($e);
+        }
+    }
+}
